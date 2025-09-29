e011a8a5a8c6ab53e5feea65499cd4e65bf437e9
masanisunil572 <sunil.m0711@gmail.com>
2025-09-29 10:17:50 +0530

    newmigrations

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "vercel-secret")
 
-    
-    # ✅ FIX: use postgresql:// instead of postgres://
+    # ✅ Correct SQLAlchemy URL (removed supa=... part)
     SQLALCHEMY_DATABASE_URI = os.getenv(
-        "Database_URL",
-        "postgresql://postgres.rgasykfchkmuxacuopso:aPRzlGNG2kk19RmH@aws-1-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require")
-    
+        "DATABASE_URL",
+        "postgresql+psycopg2://postgres.nrzuezbqjswglqyjlkbc:6fpet6vS5arlNQZy@aws-1-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
